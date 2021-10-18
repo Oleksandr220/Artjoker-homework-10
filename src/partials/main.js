1390fcd54cffb774eef6bfddceb9db7dce20a59c
Oleksandr220 <74794729+Oleksandr220@users.noreply.github.com>
2021-10-19 02:09:39 +0300

    2

--- src/partials/main.js
+++ src/partials/main.js
@@ -1,42 +1,56 @@
 "use strict";
 
-import { users } from "./users";
-
 const userList = document.querySelector(".users");
+const formAccounts = document.querySelector(".form__accounts");
 
-userList.innerHTML =
-  "<ul class=class__list>" +
-  users
-    .map(
-      ({ name, currency, regDate, finalDate, account }) =>
-        "<li class=class__list--item>" +
-        name +
-        "<br>" +
-        "Валюта: " +
-        currency +
-        "<br>" +
-        "От: " +
-        regDate +
-        "<br>" +
-        "До: " +
-        finalDate +
-        "<br>" +
-        "Дебитовый счет: " +
-        account.debitAccount +
-        currency +
-        "<br>" +
-        "Кредитовый счет: " +
-        account.creditAccount.personalFound +
-        currency +
-        "<br>" +
-        "Кредитные средства: " +
-        account.creditAccount.creditFounds.founds +
-        currency +
-        "<br>" +
-        "Кредитный лимит: " +
-        account.creditAccount.creditFounds.limit +
-        currency +
-        "</li>"
-    )
-    .join("<br>") +
-  "</ul>";
+formAccounts.addEventListener("submit", choiceAccounts);
+
+import { users } from "./users";
+import usersMurkup from "./users.hbs";
+
+const localUsers = localStorage.getItem("users");
+const parseLocalUsers = JSON.parse(localUsers);
+
+function choiceAccounts(event) {
+  event.preventDefault();
+  const arrayAccounts = [];
+  const curAccounts = formAccounts.bankUsers.value;
+  parseLocalUsers.map((user) => {
+    if (curAccounts == "activeAccount") {
+      if (user.isActive == true) {
+        return arrayAccounts.push(user);
+      }
+    }
+    if (curAccounts == "disabledAccount") {
+      if (user.isActive == false) {
+        return arrayAccounts.push(user);
+      }
+    }
+    if (curAccounts == "allAccount") {
+      return arrayAccounts.push(user);
+    }
+  });
+  console.log(arrayAccounts);
+}
+
+function render(users) {
+  if (localUsers !== null) {
+    addUsersToMarkup(parseLocalUsers);
+  } else {
+    setUsersToLS(users);
+    addUsersToMarkup(users);
+  }
+}
+
+render(users);
+
+function setUsersToLS(clients) {
+  localStorage.setItem("users", JSON.stringify(clients));
+}
+
+function addUsersToMarkup(users) {
+  users.map((user) => {
+    const userMurkUp = usersMurkup({ user });
+    return userList.insertAdjacentHTML("beforeend", userMurkUp);
+  });
+}
