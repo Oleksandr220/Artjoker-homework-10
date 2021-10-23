a83de4f515f92f086aec046b8a192835707a39ea
Oleksandr220 <74794729+Oleksandr220@users.noreply.github.com>
2021-10-23 21:36:34 +0300

    update main.js

--- src/partials/main.js
+++ src/partials/main.js
@@ -9,6 +9,8 @@ const userList = document.querySelector(".users__list");
 const formAccounts = document.querySelector(".form__accounts");
 const totalCount = document.getElementById("total-count");
 const creditCount = document.getElementById("credit-count");
+document.addEventListener("DOMContentLoaded", setUsersToLS(users));
+document.addEventListener("DOMContentLoaded", render(users, "allAccount"));
 formAccounts.addEventListener("submit", choiceAccounts);
 
 const localUsers = localStorage.getItem("users");
@@ -16,16 +18,17 @@ const parseLocalUsers = JSON.parse(localUsers);
 
 function choiceAccounts(event) {
   event.preventDefault();
-  const arrayAccounts = [];
   const curAccounts = formAccounts.bankUsers.value;
+  const arrayAccounts = [];
+
   parseLocalUsers.map((user) => {
     if (curAccounts == "activeAccount") {
-      if (user.isActive == true) {
+      if (user.isActive === true) {
         arrayAccounts.push(user);
       }
     }
     if (curAccounts == "disabledAccount") {
-      if (user.isActive == false) {
+      if (user.isActive === false) {
         arrayAccounts.push(user);
       }
     }
@@ -33,22 +36,25 @@ function choiceAccounts(event) {
       arrayAccounts.push(user);
     }
   });
-  render(arrayAccounts);
+  render(arrayAccounts, curAccounts);
 }
 // Рендер
-function render(users) {
+function render(users, curAccounts) {
   if (localUsers === null) {
     return addUsersToMarkup(parseLocalUsers);
   }
   addUsersToMarkup(users);
-  totalBankFounds(users);
+  onTotalAmountOdDebt(users, curAccounts);
 }
 
 // Добавление юзеров в localStorage
 function setUsersToLS(clients) {
+  if (parseLocalUsers !== undefined) {
+    console.log(parseLocalUsers);
+    return;
+  }
   localStorage.setItem("users", JSON.stringify(clients));
 }
-setUsersToLS(users);
 
 // Создание разметки по шаблону
 function addUsersToMarkup(users) {
@@ -61,29 +67,50 @@ function addUsersToMarkup(users) {
 }
 
 // Подсчет общего количества денег в банке
-async function totalBankFounds(clients) {
+async function onTotalAmountOdDebt(clients, curAccounts) {
   let totalFounds = 0;
   let amountOfDebt = 0;
-
-  for (let client of clients) {
-    const fetches = await fetchApi(client.currency);
-    totalFounds += Math.floor(
-      client.account.debitAccount +
-        client.account.creditAccount.personalFound +
+  if (curAccounts === "allAccount") {
+    for (let client of clients) {
+      const fetches = await fetchApi(client.currency);
+      totalFounds += Math.floor(
+        (client.account.debitAccount +
+          client.account.creditAccount.personalFound +
+          (client.account.creditAccount.creditFounds.limit -
+            client.account.creditAccount.creditFounds.founds)) /
+          fetches
+      );
+      amountOfDebt += Math.floor(
         (client.account.creditAccount.creditFounds.limit -
           client.account.creditAccount.creditFounds.founds) /
           fetches
-    );
-    amountOfDebt += Math.floor(
-      (client.account.creditAccount.creditFounds.limit -
-        client.account.creditAccount.creditFounds.founds) /
-        fetches
-    );
-    // console.log("fetches", fetches);
-    // console.log("totalFounds:", totalFounds);
-    // console.log("amountOfDebt:", amountOfDebt);
+      );
+    }
+    totalCount.innerHTML = `Общая сумма денег в банке: ${totalFounds} USD`;
+    creditCount.innerHTML = `Общая сумма долга перед банком всех клиентов: ${amountOfDebt} USD`;
+    return;
   }
-
-  totalCount.innerHTML = `Общая сумма денег в банке: ${totalFounds}`;
-  creditCount.innerHTML = `Общая сумма долга перед банком: ${amountOfDebt}`;
+  if (curAccounts === "activeAccount") {
+    for (let client of clients) {
+      const fetches = await fetchApi(client.currency);
+      amountOfDebt += Math.floor(
+        (client.account.creditAccount.creditFounds.limit -
+          client.account.creditAccount.creditFounds.founds) /
+          fetches
+      );
+    }
+    creditCount.innerHTML = `Общая сумма долга перед банком активных клиентов: ${amountOfDebt} USD`;
+    return;
+  }
+  if (curAccounts === "disabledAccount") {
+    for (let client of clients) {
+      const fetches = await fetchApi(client.currency);
+      amountOfDebt += Math.floor(
+        (client.account.creditAccount.creditFounds.limit -
+          client.account.creditAccount.creditFounds.founds) /
+          fetches
+      );
+    }
+    creditCount.innerHTML = `Общая сумма долга перед банком не активных клиентов: ${amountOfDebt} USD`;
+  }
 }
